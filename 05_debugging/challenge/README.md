0a406922a0216b4114dae18535a5e0cb47a95ba1
hakareem <harithakareem72@gmail.com>
2022-08-31 10:25:32 +0100

    post requests

---
+++ 05_debugging/challenge/README.md
@@ -0,0 +1,22 @@
+# (Broken) Blog app
+
+## User stories
+
+```
+As a developer who likes to blog,
+So I can write about my learnings,
+I'd like to add a new post on the blog.
+```
+
+```
+As a developer who likes to read,
+So I can read about cool tech things,
+I'd like to browse the list of blog posts.
+```
+
+```
+As a developer who likes to read,
+So I can read about the things I'm interested with,
+I'd like to browse the list of blog posts having a specific tag.
+```
+
---
+++ 05_debugging/challenge/app.rb
@@ -0,0 +1,45 @@
+require 'sinatra'
+require "sinatra/reloader"
+require_relative 'lib/post.rb'
+require_relative 'lib/post_manager.rb'
+
+class Application < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
+    also_reload 'lib/post.rb'
+    also_reload 'lib/post_manager.rb'
+  end
+
+  before do
+    @post_manager = PostManager.instance
+  end
+
+  get '/' do
+    @posts = @post_manager.all_posts
+
+    return erb(:index)
+  end
+
+  get '/tag/:tag' do
+    tag = params[:tag]
+    @posts = @post_manager.all_posts_by_tag(tag)
+    erb :index
+  end
+
+  # get posts for a given tag
+  post '/tag/:tag' do
+    tag = params[:tag]
+    @posts = @post_manager.all_posts_by_tag(tag)
+    p @posts
+
+    return erb(:index)
+  end
+
+  # create new post
+  post '/posts' do
+    new_post = Post.new(params[:title], params[:content], params[:tags].split(','))
+    @post_manager.add_post(new_post)
+
+    return redirect('/')
+  end
+end
